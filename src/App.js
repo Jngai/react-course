373468426da1c67ffdd3582591dbd65e56298419
Judy Ngai <judy.ngai1297@gmail.com>
2020-05-27 13:34:15 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: lightgreen;
-    color: black;
-  }
-`;
 
 class App extends Component {
 
@@ -115,9 +100,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm a react app</h1>
         <p className={classes.join(' ')}>This is really working</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+        <button className="button" onClick={this.togglePersonsHandler}>
           Toggle Person
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
